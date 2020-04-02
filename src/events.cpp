d57ac4f92795243f24d3cbe84dd063cbc8b8b448
tyndy125 <61711535+tyndy125@users.noreply.github.com>
2020-04-02 17:58:48 +0200

    Update events.cpp

--- src/events.cpp
+++ src/events.cpp
@@ -136,6 +136,8 @@ bool Events::load()
 				info.playerClearImbuement = event;
 			}else if (methodName == "onCombat") {
 				info.playerOnCombat = event;
+        } else if (methodName == "onInventoryUpdate") {
+				info.playerOnInventoryUpdate = event;
 
 			} else {
 				std::cout << "[Warning - Events::load] Unknown player method: " << methodName << std::endl;
@@ -1089,6 +1091,37 @@ void Events::eventPlayerOnCombat(Player* player, Creature* target, Item* item, C
 
 	scriptInterface.resetScriptEnv();
 }
+
+void Events::eventPlayerOnInventoryUpdate(Player* player, Item* item, slots_t slot, bool equip)
+{
+	// Player:onInventoryUpdate(item, slot, equip)
+	if (info.playerOnInventoryUpdate == -1) {
+		return;
+	}
+
+	if (!scriptInterface.reserveScriptEnv()) {
+		std::cout << "[Error - Events::eventPlayerOnInventoryUpdate] Call stack overflow" << std::endl;
+		return;
+	}
+
+	ScriptEnvironment* env = scriptInterface.getScriptEnv();
+	env->setScriptId(info.playerOnInventoryUpdate, &scriptInterface);
+
+	lua_State* L = scriptInterface.getLuaState();
+	scriptInterface.pushFunction(info.playerOnInventoryUpdate);
+
+	LuaScriptInterface::pushUserdata<Player>(L, player);
+	LuaScriptInterface::setMetatable(L, -1, "Player");
+
+	LuaScriptInterface::pushUserdata<Item>(L, item);
+	LuaScriptInterface::setItemMetatable(L, -1, item);
+
+	lua_pushnumber(L, slot);
+	LuaScriptInterface::pushBoolean(L, equip);
+
+	scriptInterface.callVoidFunction(4);
+}
+
 
 void Events::eventMonsterOnDropLoot(Monster* monster, Container* corpse)
 {
